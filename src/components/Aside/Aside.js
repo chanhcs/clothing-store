841bf267cc999aa26be3c14a59d9ca8fc66f7083
Phạm Minh Chánh <chanhpm.cs@gmail.com>
2019-07-14 20:58:53 +0700

    add modal

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -4,6 +4,7 @@ import ReactSwipe from 'react-swipe';
 import img1 from './img1.jpg';
 import img2 from './img2.jpg';
 import img3 from './img3.jpg';
+import Messenger from './messenger.svg'
 import ArrowLeft from './arrowLeft.svg';
 import ArrowRight from './arrowRight.svg';
 
@@ -14,7 +15,7 @@ const aside = () => {
     <div>
 
       <h6>Có thể bạn quan tâm</h6>
-      <button className="btn btn-danger" onClick={() => reactSwipeEl.prev()} style={{ marginRight: '5px'}}>
+      <button className="btn btn-danger" onClick={() => reactSwipeEl.prev()} style={{ marginRight: '5px' }}>
         <img src={ArrowLeft} alt='arrowLeft' style={{ width: '20px', height: '20px' }} />
       </button>
       <button className="btn btn-danger" onClick={() => reactSwipeEl.next()}>
@@ -32,8 +33,36 @@ const aside = () => {
         <img src={img3} alt='img3' />
 
       </ReactSwipe>
-      <button className="btn btn-primary">Mua ngay</button>
-     <hr></hr>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Mua hàng
+</button>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Mua hàng</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              Để mua sản phẩm vui lòng liên hệ trực tiếp với
+              <button className="btn btn-primary">
+                <a target="Blank" href='https://m.me/pmchanhpk' tabIndex="0" style={{ textDecoration: 'none', color: 'white' }}>
+                  <img src={Messenger} alt='Messenger' style={{width: '30px', height: '50px'}}/> Phạm Minh Chánh
+                </a>
+              </button>
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
     </div>
   );
 };
